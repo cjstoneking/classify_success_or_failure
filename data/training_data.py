fc92d541d5307ddfb6876c80ec3c889bf470bb73
CJ Stoneking <cjstoneking@gmail.com>
2019-08-03 08:50:57 -0500

    Update training_data.py

--- data/training_data.py
+++ data/training_data.py
@@ -1,1 +1,34 @@
 
+#append <the project> was a/turned out to be/resulted in a
+negative_outcomes = {
+  "hopelessly over budget":0,\
+  "dead in the water":0,\
+  "dead on arrival":0,\
+  "disaster":0,\
+  "catastrophe":0,\
+  "train wreck":0,\
+  "failure":0
+  "complete failure":0,\
+  "mess":0,\
+  "total mess":0,\
+  "disappointment":0,\
+  "waste of time":0\
+}
+
+unique_positive_statements = {
+  "{pron} managed to succeed":1,\
+  "{pron} managed to make it":1,\
+  "{sub} succeeded in the end":1,\
+  "{sub} did make some money":1,\
+  "{poss} hard work paid off":1
+}
+
+wikipedia_sentences = {
+  "The disaster resulted in a 32-month hiatus in the shuttle program":0,\
+  "The spacecraft disintegrated over the Atlantic Ocean":0,\
+  "Marshall and Thiokol realized that they had a potentially catastrophic problem on their hands":0,\
+  
+}
+  
+  
+  
