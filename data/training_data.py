efe1f82257fa7b325cb8bfca1b1db343d70548ca
CJ Stoneking <cjstoneking@gmail.com>
2019-08-19 08:39:26 -0400

    Update training_data.py

--- data/training_data.py
+++ data/training_data.py
@@ -1,12 +1,12 @@
 
 #append <the project> was a/turned out to be/resulted in a
 negative_outcomes = {
-  "hopelessly over budget":0,\
-  "dead in the water":0,\
-  "dead on arrival":0,\
-  "disaster":0,\
-  "catastrophe":0,\
-  "train wreck":0,\
+  "{sub} was hopelessly over budget":0,\
+  "{sub} was dead in the water":0,\
+  "{sub} was dead on arrival":0,\
+  "{sub} was a disaster":0,\
+  "{sub} was a catastrophe":0,\
+  "{sub} was a train wreck":0,\
   "failure":0
   "complete failure":0,\
   "mess":0,\
@@ -23,11 +23,26 @@ unique_positive_statements = {
   "{poss} hard work paid off":1
 }
 
-wikipedia_sentences = {
+#these are taken from:
+# wikipedia
+# https://interestingengineering.com/23-engineering-disasters-of-all-time
+full_sentences = {
   "The disaster resulted in a 32-month hiatus in the shuttle program":0,\
   "The spacecraft disintegrated over the Atlantic Ocean":0,\
   "Marshall and Thiokol realized that they had a potentially catastrophic problem on their hands":0,\
-  
+  "Hindenburg was an airship made in Germany and led to a big disaster that killed 36 passengers including crew members":0,\
+  "The airship caught fire and also crashed while trying to dock itself in New Jersey":0,\
+  "The Quebec Bridge collapsed twice in Canada":0,\
+  "This disaster is known to have killed as many as 88-89 workers":0,\
+  "It was a big disaster for them, and they thought they had learned their lesson":0,\
+  "Yet again, disaster struck in 1916 and the central span of the bridge crumbled down":0,\
+  "This time, it killed 13 workers":0,\
+  "Titanic is one of the most well-known engineering disasters in the world that claimed the lives of more than 1500 people onboard":0,\
+  "In 1986, nuclear reactors failed and led to a series of explosions and radioactive fallout":0,\
+  "":0,\
+  "":0,\
+  "":0,\
+  "":0,\
 }
   
   
