b9de1edd02dd9305d9ff03e245e6bc6889a9c1b6
CJ Stoneking <cjstoneking@gmail.com>
2019-09-05 15:51:31 -0400

    Add files via upload

---
+++ training_neutral_urls.py
@@ -0,0 +1,29 @@
+
+#these URLs are the sources of neutral (no failure or success) training sentences
+
+training_neutral_urls = {\
+                          "https://en.wikipedia.org/wiki/Linux":0,\
+                          "https://en.wikipedia.org/wiki/Triborough_Bridge":0,\
+                          "https://en.wikipedia.org/wiki/Database":0,\
+                          "https://en.wikipedia.org/wiki/Lean_startup":0,\
+                          "https://en.wikipedia.org/wiki/Business_model":0,\
+                          "https://en.wikipedia.org/wiki/Pinterest":0,\
+                          "https://en.wikipedia.org/wiki/Twitter":0,\
+                          "https://en.wikipedia.org/wiki/Application_software":0,\
+                          "https://en.wikipedia.org/wiki/Web_search_engine":0,\
+                          "https://en.wikipedia.org/wiki/Software":0,\
+                          "https://en.wikipedia.org/wiki/Project":0,\
+                          "https://en.wikipedia.org/wiki/Corporation":0,\
+                          "https://en.wikipedia.org/wiki/Engineering":0,\
+                          "https://en.wikipedia.org/wiki/Firefox":0,\
+                          "https://en.wikipedia.org/wiki/User_interface":0,\
+                          "https://en.wikipedia.org/wiki/Operating_system":0,\
+                          "https://en.wikipedia.org/wiki/Adobe_Photoshop":0,\
+                          "https://en.wikipedia.org/wiki/IPad":0,\
+                          "https://en.wikipedia.org/wiki/Coursera":0,\
+                          "https://en.wikipedia.org/wiki/Supercomputer":0,\
+                          "https://en.wikipedia.org/wiki/Skype":0,\
+                          "https://en.wikipedia.org/wiki/Wikipedia":0,\
+                          "https://en.wikipedia.org/wiki/Software_as_a_service":0,\
+                          "https://en.wikipedia.org/wiki/Computer-aided_design":0
+                          }
